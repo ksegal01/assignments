ac52f4eb8c258b82f267b1198c2ce6868e98fb31
KATIE SEGAL <KATIE_SEGAL@STUDENT.HKS.HARVARD.EDU>
2019-01-09 22:50:05 -0500

    made a javascript calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function multiply (a,b) {
+	return a * b // The function multiplies a times b
+}
+
+function divide (a,b){
+	return a / b // This function divides a by b
+}
+
+function add (a,b){
+	return a + b //and this function adds a and b
+}
+
+function subtract (a,b){
+	return a - b //this last function subtracts b from a
+}
