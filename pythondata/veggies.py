43602537e2a5e76a40e8aa5a9df0f184c9012cd7
KATIE SEGAL <KATIE_SEGAL@STUDENT.HKS.HARVARD.EDU>
2019-01-17 11:26:20 -0500

    adds vegetable exercise

---
+++ pythondata/veggies.py
@@ -0,0 +1,24 @@
+#Write the vegetable to a CSV file
+import csv
+
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "cauliflower", "color": "white"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+with open('vegetables.csv', 'w') as f:
+	writer = csv.writer(f)
+	#write header
+	writer.writerow(['name', 'color', 'length_of_name'])
+	#write data
+
+	# Loop through each vegetable
+	for vegetable in vegetables:
+		name = vegetable['name']
+		color = vegetable['color']
+		length_of_name = len(vegetable['name'])
+		writer.writerow([name,color])
