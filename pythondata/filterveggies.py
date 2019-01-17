16278308e725de1549c4154a78410fe1a4ff3fb1
KATIE SEGAL <KATIE_SEGAL@STUDENT.HKS.HARVARD.EDU>
2019-01-17 15:04:43 -0500

    adds python filtering lesson with veggies

---
+++ pythondata/filterveggies.py
@@ -0,0 +1,23 @@
+#LISA S. & KATIE S.
+import csv
+import json
+from pprint import pprint
+#Read vegetables.csv into a variable called vegetables.
+with open('vegetables.csv', 'r') as f :
+	reader = csv.DictReader(f)
+	vegetables = [dict(vegetable) for vegetable in reader]
+
+# Loop through vegetables and filter down to only green vegtables using a whitelist.
+green_veggies = []
+whitelist = ['okra', 'arugula', 'broccoli']
+for vegetable in vegetables:
+	if vegetable['name'] in whitelist:
+		green_veggies.append(vegetable)
+
+
+# Print veggies to the terminal
+pprint(green_veggies)
+
+# Write the veggies to a json file called greenveggies.json
+with open ('greenveggies.json', 'w') as f:
+	json.dump(green_veggies, f, indent=2)
